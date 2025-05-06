37dcf97a127aa8591575b04b255f4cec69713fe2
SKYRENZ <131676044+SKYRENZ@users.noreply.github.com>
2025-05-06 11:49:18 +0800

    set up
    using VIte, React - ts, MUI

---
+++ frontend/src/theme/typography.ts
@@ -0,0 +1,17 @@
+const typography = {
+    fontFamily: "'Roboto', sans-serif",
+    h1: {
+      fontSize: "2rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: 600,
+    },
+    body1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+    },
+  };
+  
+  export default typography;
